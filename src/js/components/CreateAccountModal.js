5d7b56c298d179ea358d20fe4d22bff4d59d25f4
Emerson <emerson.matosokl10@gmail.com>
2024-01-14 18:08:49 -0300

    Crate modal

---
+++ src/js/components/CreateAccountModal.js
@@ -0,0 +1,22 @@
+// src/js/Modal.js
+export default function Modal() {
+    return `
+        <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-xl">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Adicionar uma conta</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Adicionar uma conta
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                        <button type="button" class="btn btn-primary">Adicionar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+}
